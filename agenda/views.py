0848bd38ac823458bc2d12a544200bfb83ae6624
Joseph Ortiz <Se_sema_95@hotmail.com>
2024-10-31 16:52:52 +0000

    Add templates

--- agenda/views.py
+++ agenda/views.py
@@ -4,6 +4,6 @@ from .models import Event
 
 # Create your views here.
 class EventList(generic.ListView):
-    queryset = Event.objects.filter(status=1).order_by("-event_date")
+    queryset = Event.objects.filter(status=1).order_by("event_date")
     template_name = "agenda/index.html"
     paginate_by = 2
