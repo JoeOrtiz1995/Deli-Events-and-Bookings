f19264a0c9d00419cca6921dbc457eabafa7b536
Joseph Ortiz <Se_sema_95@hotmail.com>
2024-10-31 13:43:44 +0000

    Add Event Model

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Unconfirmed"), (1, "Approved"))
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    excerpt = models.SlugField(max_length=200, unique=True)
+    organiser = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="active_events"
+    )
+    description = models.TextField()
+    event_date = models.DateTimeField(auto_now_add=False)
+    status = models.IntegerField(choices=STATUS, default=0)
