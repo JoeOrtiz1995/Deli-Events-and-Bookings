765a30d780eac40b02164574159a9125d260cb59
Joseph Ortiz <Se_sema_95@hotmail.com>
2024-10-31 15:54:33 +0000

    Install Summernote and check is visible in admin panel

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Event, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'event_date')
+    search_fields = ['title', 'event_date']
+    list_filter = ('title', 'event_date',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description', 'excerpt',)    
 
 # Register your models here.
-admin.site.register(Event)
-
 admin.site.register(Comment)
--- deli/urls.py
+++ deli/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('agenda.urls'), name='agenda-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('agenda.urls'), name='agenda-urls'),
 ]
