649a455cc0caba94bb3bbac19b3f4540daf5d510
Joseph Ortiz <Se_sema_95@hotmail.com>
2024-10-31 18:28:43 +0000

    Add detailed view of events

--- agenda/views.py
+++ agenda/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Event
 
@@ -7,3 +7,17 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by("event_date")
     template_name = "agenda/index.html"
     paginate_by = 2
+
+def event_detail(request, slug):
+    """
+    Displays just one post in detail
+    """
+
+    queryset = Event.objects.filter(status=1)
+    event = get_object_or_404(queryset, slug = slug)
+
+    return render(
+        request,
+        "agenda/event_detail.html",
+        {"event": event},
+    )
