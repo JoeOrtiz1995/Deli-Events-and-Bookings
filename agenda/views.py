365c580bb9c8c231c0182feddb0d8f7c643a018d
Joseph Ortiz <Se_sema_95@hotmail.com>
2024-11-01 16:42:46 +0000

    Add comments section

--- agenda/views.py
+++ agenda/views.py
@@ -10,14 +10,17 @@ class EventList(generic.ListView):
 
 def event_detail(request, slug):
     """
-    Displays just one post in detail
+    Displays just one post in detail with the comments section below
     """
 
     queryset = Event.objects.filter(status=1)
     event = get_object_or_404(queryset, slug = slug)
+    comments = event.comments.filter(approved=True).order_by("-created_on")
 
     return render(
         request,
         "agenda/event_detail.html",
-        {"event": event},
+        {"event": event,
+        "comments": comments,        
+        },
     )
